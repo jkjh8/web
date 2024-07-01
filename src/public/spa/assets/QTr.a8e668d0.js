f8bc13a84b3615e2a7eae9aac4459e7d5a989b25
jkjh8 <jkjh82@naver.com>
2024-07-01 22:45:53 +0900

    chore: Update i18n, register, useScheduleFunc, and QTr assets to use latest version

--- src/public/spa/assets/QTr.a8e668d0.js
+++
@@ -1,1 +0,0 @@
-import{u as t,c as s,h as a,y as n}from"./index.13719b77.js";var u=t({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const o=s(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>a("tr",{class:o.value},n(e.default))}});export{u as Q};
